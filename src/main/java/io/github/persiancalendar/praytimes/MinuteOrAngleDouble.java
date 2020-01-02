4d7dd32d425e5b531a9e0cdf27723333fb4651c3
Hamidreza Bayat <Hamidrezabayat76@gmail.com>
2020-01-02 16:31:38 +0330

    + Java project

---
+++ src/main/java/io/github/persiancalendar/praytimes/MinuteOrAngleDouble.java
@@ -0,0 +1,19 @@
+package io.github.persiancalendar.praytimes;
+
+public class MinuteOrAngleDouble {
+    private final boolean isMin;
+    private final double value;
+
+    MinuteOrAngleDouble(double value, boolean isMinute) {
+        this.value = value;
+        this.isMin = isMinute;
+    }
+
+    boolean isMinute() {
+        return isMin;
+    }
+
+    public double getValue() {
+        return value;
+    }
+}
