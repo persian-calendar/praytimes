89929c0783814cb5f08057e56f58038391d36d16
Ebrahim Byagowi <ebrahim@gnu.org>
2021-10-08 17:30:12 +0330

    Don't expose calculation method name
    We can't provide i18n for this so let's remove it.

--- src/main/java/io/github/persiancalendar/praytimes/CalculationMethod.java
+++ src/main/java/io/github/persiancalendar/praytimes/CalculationMethod.java
@@ -4,44 +4,40 @@ import static io.github.persiancalendar.praytimes.Utils.deg;
 import static io.github.persiancalendar.praytimes.Utils.min;
 
 public enum CalculationMethod {
-    MWL("Muslim World League", deg(18), deg(17)),
-    ISNA("Islamic Society of North America (ISNA)", deg(15), deg(15)),
-    Egypt("Egyptian General Authority of Survey", deg(19.5), deg(17.5)),
-    Makkah("Umm Al-Qura University, Makkah", deg(18.5), min(90)),
-    Karachi("University of Islamic Sciences, Karachi", deg(18), deg(18)),
-    Tehran("Institute of Geophysics, University of Tehran", deg(17.7), deg(14), deg(4.5),
-            MidnightType.Jafari),
-    Jafari("Shia Ithna-Ashari, Leva Institute, Qum", deg(16), deg(14), deg(4),
-            MidnightType.Jafari);
+    // Muslim World League
+    MWL(deg(18), deg(17)),
+    // Islamic Society of North America (ISNA)
+    ISNA(deg(15), deg(15)),
+    // Egyptian General Authority of Survey
+    Egypt(deg(19.5), deg(17.5)),
+    // Umm Al-Qura University, Makkah
+    Makkah(deg(18.5), min(90)),
+    // University of Islamic Sciences, Karachi
+    Karachi(deg(18), deg(18)),
+    // Institute of Geophysics, University of Tehran
+    Tehran(deg(17.7), deg(14), deg(4.5), MidnightType.Jafari),
+    // Shia Ithna-Ashari, Leva Institute, Qum
+    Jafari(deg(16), deg(14), deg(4), MidnightType.Jafari);
 
-    private final String name;
     private final MinuteOrAngleDouble fajr;
     private final MinuteOrAngleDouble isha;
     private final MinuteOrAngleDouble maghrib;
     private final MidnightType midnight;
 
-    CalculationMethod(String name, MinuteOrAngleDouble fajr,
-                      MinuteOrAngleDouble isha, MinuteOrAngleDouble maghrib,
+    CalculationMethod(MinuteOrAngleDouble fajr, MinuteOrAngleDouble isha, MinuteOrAngleDouble maghrib,
                       MidnightType midnight) {
-        this.name = name;
         this.fajr = fajr;
         this.isha = isha;
         this.maghrib = maghrib == null ? min(0) : maghrib;
         this.midnight = midnight == null ? MidnightType.Standard : midnight;
     }
 
-    CalculationMethod(String name, MinuteOrAngleDouble fajr,
-                      MinuteOrAngleDouble isha, MinuteOrAngleDouble maghrib) {
-        this(name, fajr, isha, maghrib, null);
+    CalculationMethod(MinuteOrAngleDouble fajr, MinuteOrAngleDouble isha, MinuteOrAngleDouble maghrib) {
+        this(fajr, isha, maghrib, null);
     }
 
-    CalculationMethod(String name, MinuteOrAngleDouble fajr,
-                      MinuteOrAngleDouble isha) {
-        this(name, fajr, isha, null);
-    }
-
-    public String getName() {
-        return name;
+    CalculationMethod(MinuteOrAngleDouble fajr, MinuteOrAngleDouble isha) {
+        this(fajr, isha, null);
     }
 
     public MinuteOrAngleDouble getFajr() {
