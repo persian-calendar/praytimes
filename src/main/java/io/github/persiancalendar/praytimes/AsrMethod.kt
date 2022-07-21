526f2643c4af5da0fa656e9cabcb10d9b71c9852
Hamidreza Bayat <hamidrezabayat76@gmail.com>
2022-07-21 13:07:46 +0430

    AsrMethod.kt Java interop

--- src/main/java/io/github/persiancalendar/praytimes/AsrMethod.kt
+++ src/main/java/io/github/persiancalendar/praytimes/AsrMethod.kt
@@ -3,7 +3,7 @@ package io.github.persiancalendar.praytimes
 // Asr Juristic Methods
 enum class AsrMethod(  // Hanafi
     // asr shadow factor
-    val asrFactor: Double
+    @JvmField val asrFactor: Double
 ) {
     Standard(1.0),  // Shafi`i, Maliki, Ja`fari, Hanbali
     Hanafi(2.0);
